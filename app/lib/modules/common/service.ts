ce32f21b97fd60dbc8a2175df7bb323533e78914
Moumen <wifi.moumen@gmail.com>
2020-12-05 12:29:45 +0100

    Return all currencies if no country is set

--- app/lib/modules/common/service.ts
+++ app/lib/modules/common/service.ts
@@ -2,19 +2,11 @@ import {Response} from "express";
 import {responseStatusCode} from "./model";
 
 export function successResponse(message: string, data: any, res: Response) {
-    res.status(responseStatusCode.success).json({
-        status: 'success',
-        message,
-        data,
-    })
+    res.status(responseStatusCode.success).json({data})
 }
 
 export function failureResponse(message: string, data: any, res: Response) {
-    res.status(responseStatusCode.success).json({
-        status: 'failure',
-        message,
-        data,
-    })
+    res.status(responseStatusCode.success).json({status: 'failure', message})
 }
 
 export function insufficientParams(res: Response) {
--- app/lib/modules/currency/service.ts
+++ app/lib/modules/currency/service.ts
@@ -4,8 +4,7 @@ export default class CurrencyService {
 
     public getCurrencyByCountry(country: string) {
         // get the default one if no country is selected
-        console.log(country);
-        if (!country) return currencies[0];
+        if (!country) return currencies;
         // filter the list of currencies depending on the country
         return currencies.filter(item => item.countries.includes(country));
     }
